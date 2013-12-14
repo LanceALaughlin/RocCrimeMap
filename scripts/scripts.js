56edb65d5cc440838ff692221e17e2c40bd1ea6a
ExplosiveHippo <lancealaughlin@gmail.com>
2013-12-14 11:56:26 -0500

    Fixed the issue which prevented all the data being loaded sometimes by plotting the crime points before loading the shape file. Also changed the order of the data in the side bar to make more sense

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -43,8 +43,8 @@ function init(){
     
     //console.log(queryString);
     loadData(queryString);*/
-    plotNeighborhoods();
     loadData();
+    plotNeighborhoods();
 }
 
 
@@ -129,8 +129,8 @@ function generateCrimeData(crimeID,coords){
 					var description = response[n].Description;
 					
 					var string = '<h1>'+address+'</h1>';
-					string += '<h2>'+agency+'</h2>';
-					string += '<h3>'+type+'</h3>';
+					string += '<h2>'+type+'</h2>';
+					string += '<h3>'+agency+'</h3>';
 					string += '<p>Date and Time:'+time+'</p>';
 					string += '<p>'+description+'</p>';
 					string += '<a href="http://maps.googleapis.com/maps/api/streetview?size=800x400&location='+this.streetCoords[0]+','+this.streetCoords[1]+'&fov=90&heading=235&pitch=10&sensor=false"><img id="streetView" src="http://maps.googleapis.com/maps/api/streetview?size=400x200&location='+this.streetCoords[0]+','+this.streetCoords[1]+'&fov=90&heading=235&pitch=10&sensor=false" /></a>';
@@ -155,6 +155,7 @@ function generateCrimeData(crimeID,coords){
 function plotNeighborhoods(){
 	var shpfile = new L.Shapefile('Neighborhoods.zip',{onEachFeature:function(feature, layer) {
     	console.log(layer);
+    	layer.options.color = "#FF1900";
 	}});
          shpfile.addTo(map);
 }
